a20db5a1cbbf4ab15a0d0d131667e3fff08843e0
PappaMar <mairigpappa@gmail.com>
2023-12-08 13:00:35 +0200

    laaa

--- webfiles/stratego.php
+++ webfiles/stratego.php
@@ -1,12 +1,15 @@
 <?php
-require_once "../library/dbconnect.php";
-require_once "../library/board.php";
-require_once "../library/game.php";
+//require_once "../library/dbconnect.php";
+//require_once "../library/board.php";
+//require_once "../library/game.php";
 
 $method = $_SERVER['REQUEST_METHOD'];
-$request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
+//$request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
+$request = explode('/', trim($_SERVER['SCRIPT_NAME'],'/'));
 $input = json_decode(file_get_contents('php://input'),true);
 
+print_r($_SERVER);
+exit;
 
 switch ($r=array_shift($request)) 
 {
